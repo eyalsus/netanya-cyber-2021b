38f461581fb03220c5c9771a64d4111adf8840b0
Eyal Paz <eyalsus@gmail.com>
2021-03-09 21:27:29 +0200

    lesson 1 - part 2

--- lesson1/app.py
+++ lesson1/app.py
@@ -7,8 +7,21 @@ user_database = [
     {'username': 'bob', 'password': '2345'}
 ]
 
+@app.route('/enter/')
+def enter(name=None):
+    return render_template('login.html', name=name)
 
-# @app.route('/login'):
+@app.route('/login')
+def login():
+    response = 'ACCESS DENIED'
+    username = request.args.get('username')
+    password = request.args.get('password')
+    for user in user_database:
+        if username == user['username'] and password == user['password']:
+            response = f'Hello {username}'
+            break
+    return response
+
 
 
 @app.route('/')
