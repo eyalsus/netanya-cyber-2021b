7c60267089eb112ad9da27c777a97e52aac49998
Eyal Paz <eyalsus@gmail.com>
2021-04-06 17:57:41 +0300

    lesson 3 start

---
+++ lesson3/1.sql
@@ -0,0 +1,9 @@
+select count(1) from users where username = '{username}' and password = '{password}'
+select count(1) from users where username =  'alice' and password = '' or 1>0 --'
+
+select favorite_number from users where username = '{username}'
+
+select count(1) from users where username = 'eyal' and password = '' or 1>0 --      '
+
+
+select * from users;
